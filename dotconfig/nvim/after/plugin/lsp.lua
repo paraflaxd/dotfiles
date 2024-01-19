7cd43b7ccd1f0b88b76c5fdbf325c82dab2b0bee
paraflaxd <reallymarkus@gmail.com>
2024-01-19 21:25:41 +0100

    add harpoon

--- dotconfig/nvim/after/plugin/lsp.lua
+++ dotconfig/nvim/after/plugin/lsp.lua
@@ -13,7 +13,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<leader>h", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 -- Configure language servers
--- dotconfig/nvim/lua/paraflaxd/packer.lua
+++ dotconfig/nvim/lua/paraflaxd/packer.lua
@@ -4,6 +4,12 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { {"nvim-lua/plenary.nvim"} }
+    }
 
     use({
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
