ef4efcd7f22c9bc0a4e3e70ea158bf8e1488265b
paraflaxd <reallymarkus@gmail.com>
2024-01-20 22:54:56 +0100

    fucking tmux wont let me use Ctrl+ö

--- dotconfig/nvim/after/plugin/lsp.lua
+++ dotconfig/nvim/after/plugin/lsp.lua
@@ -5,7 +5,7 @@ lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "H", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vs", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "gn", function() vim.diagnostic.goto_next() end, opts)
